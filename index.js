f1eaee61120c2e69043211929dad57a6b6bd85f7
D M <odnajwielpiela@gmail.com>
2021-01-21 11:18:59 +0100

    added change color functionality to button

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const buttons = document.querySelectorAll(".drum");
 var soundDrum = new Audio("sounds/tom-1.mp3");
 buttons.forEach((element) => {
-    let keyType = element.innerHTML;
-    console.log(keyType);
-  element.addEventListener("click", function (){
+  let keyType = element.innerHTML;
+  console.log(keyType);
+  element.addEventListener("click", function () {
+    //element.style.color = "yellow";
+    element.classList.add("change-color");
     console.log(keyType);
   });
 });
 function clicked() {
   alert("I got clicked");
   soundDrum.play();
-//   console.log(keyType);
+  //   console.log(keyType);
 }
