cf819decd437fe485d071977ab6635d197e0a351
D M <odnajwielpiela@gmail.com>
2021-01-21 11:02:00 +0100

    taken inerhtml from button

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const buttons = document.querySelectorAll(".drum");
 var soundDrum = new Audio("sounds/tom-1.mp3");
 buttons.forEach((element) => {
-  element.addEventListener("click", clicked);
+    let keyType = element.innerHTML;
+    console.log(keyType);
+  element.addEventListener("click", function (){
+    console.log(keyType);
+  });
 });
 function clicked() {
   alert("I got clicked");
   soundDrum.play();
+//   console.log(keyType);
 }
