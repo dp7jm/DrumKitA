090e74bdb88cf88b724d2c9e28f8ada33f7647d0
D M <odnajwielpiela@gmail.com>
2021-01-21 10:32:11 +0100

    added sound after click

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const buttons = document.querySelectorAll(".drum");
+var soundDrum = new Audio("sounds/tom-1.mp3");
 buttons.forEach((element) => {
   element.addEventListener("click", clicked);
 });
 function clicked() {
   alert("I got clicked");
+  soundDrum.play();
 }
